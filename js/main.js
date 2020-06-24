6022d0242c6020ab4964e62d315a9e5c76557c5c
misaelsarm <misaelsarmiento97@gmail.com>
2020-06-24 01:36:06 -0500

    Grid gallery and other changes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ let menu = document.getElementById('menu');
 
 let navLinks = document.getElementById('nav-bar-links')
 
-
 menu.addEventListener('click', showMenu);
 let opened = false;
 function showMenu() {
@@ -11,9 +10,9 @@ function showMenu() {
     for (let i = 0; i < menu.children.length; i++) {
         menu.children[i].classList.toggle('menu-active');
         if (opened) {
-            menu.children[0].style.transform = 'rotate(-60deg) translate(-10px, 5px)';
+            menu.children[0].style.transform = 'rotate(-45deg) translate(-8px, 8px)';
             menu.children[1].style.display = 'none';
-            menu.children[2].style.transform = 'rotate(60deg)';
+            menu.children[2].style.transform = 'rotate(45deg)';
         } else {
             menu.children[0].style.transform = 'rotate(0)';
             menu.children[1].style.display = 'block';
