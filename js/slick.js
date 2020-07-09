15ea1abd45ebe560958fb301c9ec867f06f177ba
misaelsarm <misaelsarmiento97@gmail.com>
2020-07-08 22:25:32 -0500

    Cambios en diseño

---
+++ js/slick.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.header').slick({
+        autoplay: false,
+        infinite: true,
+        speed: 1000,
+        arrows: false,
+        //fade: true,
+        autoplaySpeed: 4000,
+    });
+});
